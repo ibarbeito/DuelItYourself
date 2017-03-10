0f452e58a2107f28815fed343f5f14c36894148b
ibarbeito <ismael.barbeito.vazquez@gmail.com>
2017-03-10 17:16:04 +0100

    Commit inicial

---
+++ Assets/Scripts/Duelist.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Duelist : MonoBehaviour {
+
+	public KeyCode Left;
+	public KeyCode Right;
+	public KeyCode Shoot;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(Left)) {
+			
+		}
+		if (Input.GetKey(Right)) {
+			
+		}
+		if (Input.GetKey(Shoot)) {
+
+		}
+	}
+}
