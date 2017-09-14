14aa9d685031f867afd34eb24ac447f27d5decfd
ibarbeito <ismael.barbeito.vazquez@gmail.com>
2017-09-14 20:17:23 +0200

    Me siento triste porque no me acuerdo de utilizar Unity

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public string AxisHorizontal = "Horizontal";
+	public string AxisVertical = "Vertical";
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float Horizontal = Input.GetAxis(AxisHorizontal);
+		float Vertical = Input.GetAxis(AxisVertical);
+		Debug.Log(Horizontal);
+		Vector3 Direction = new Vector3(Vertical, 0, -Horizontal);
+		GetComponent<Rigidbody>().AddForce(Direction*50f,ForceMode.VelocityChange);
+		
+	}
+}
