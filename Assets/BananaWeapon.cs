9bfee489f8da84d7175d45dea37a07de053ea6e0
JetroD <jetrodopazo48@gmail.com>
2017-09-16 22:03:11 +0200

    Commit pre merge

---
+++ Assets/BananaWeapon.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class BananaWeapon : MonoBehaviour {
+    
+    public float FireRate;
+
+    private float TimeToNextBullet = 0;
+
+    void Update()
+    {
+    }
+
+    public void Shot(GameObject shooter, Bullet Ammo)
+    {
+        if (TimeToNextBullet <= 0)
+        {
+            Bullet Bullet = Instantiate(Ammo);
+            Bullet.Owner = shooter;
+            Bullet.transform.rotation = shooter.transform.rotation;
+            Bullet.transform.position = shooter.transform.position;
+            TimeToNextBullet = FireRate;
+        }
+
+    }
+
+    public void OnUpdate()
+    {
+        TimeToNextBullet -= Time.deltaTime;
+
+    }
+
+}
+
--- Assets/GraphicJam/PlayerController.cs
+++ Assets/GraphicJam/PlayerController.cs
@@ -6,11 +6,13 @@ public class PlayerController : MonoBehaviour {
 
 	public string AxisHorizontal = "Horizontal";
 	public string AxisVertical = "Vertical";
+    public string ShootButton = "Fire1";
 	public float Speed = 10f;
+    public BananaWeapon Weapon;
+    public Bullet Bullet;
 
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
@@ -19,5 +21,12 @@ public class PlayerController : MonoBehaviour {
 		float Vertical = Input.GetAxis(AxisVertical);
 		Vector3 Direction = new Vector3(Horizontal, 0, Vertical);
 		GetComponent<Rigidbody>().AddForce(Direction*Speed,ForceMode.VelocityChange);
+        if (Input.GetButtonDown(ShootButton))
+        {
+            Bullet b = Bullet.Instantiate(Bullet);
+            b.transform.position = gameObject.transform.position;
+            Weapon.Shot(gameObject, b);
+            
+        }
 	}
 }
