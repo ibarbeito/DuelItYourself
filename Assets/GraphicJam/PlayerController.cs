d48360f40fb4653aaa5ffb000b72131251a419f2
ibarbeito <ismael.barbeito.vazquez@gmail.com>
2017-09-16 22:02:30 +0200

    Commit pre merge de Isma

--- Assets/GraphicJam/PlayerController.cs
+++ Assets/GraphicJam/PlayerController.cs
@@ -7,17 +7,34 @@ public class PlayerController : MonoBehaviour {
 	public string AxisHorizontal = "Horizontal";
 	public string AxisVertical = "Vertical";
 	public float Speed = 10f;
+	LifeManager LifeMan;
 
 	// Use this for initialization
 	void Start () {
-		
+		LifeMan = GetComponent<LifeManager>();
 	}
-	
-	// Update is called once per frame
+
+	void Update() {
+		if (Input.GetKeyUp(KeyCode.F)) {
+			Hit(1);	
+		}
+		
+	}	
+
 	void FixedUpdate () {
 		float Horizontal = Input.GetAxis(AxisHorizontal);
 		float Vertical = Input.GetAxis(AxisVertical);
 		Vector3 Direction = new Vector3(Horizontal, 0, Vertical);
 		GetComponent<Rigidbody>().AddForce(Direction*Speed,ForceMode.VelocityChange);
 	}
+
+    public void Hit(int Damage) {
+		LifeMan.DoDamage(Damage);
+        bool Alive = LifeMan.IsAlive();
+		if (!Alive) {
+           gameObject.SetActive(false);
+		}
+    }
+
+
 }
