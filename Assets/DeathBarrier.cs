364c71f230d13d1b6477a1499a42662cd394f77e
ibarbeito <ismael.barbeito.vazquez@gmail.com>
2017-09-27 20:13:18 +0200

    Cambios a scripts, menu y script de decrecer terreno

---
+++ Assets/DeathBarrier.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathBarrier : MonoBehaviour {
+
+	void OnCollisionEnter(Collision collision)
+    {
+        collision.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/DecreaseTerrain.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecreaseTerrain : MonoBehaviour {
+
+	public float DecreaseSpeed = 0.2f;
+	Vector3 Direction = new Vector3(1,0,1);
+	public Vector3 MinScale = new Vector3(1,1,1);
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 ActualScale = transform.localScale;
+		if (ActualScale.x > MinScale.x && ActualScale.z > MinScale.z)
+			transform.localScale -= DecreaseSpeed*Direction;
+	}
+}
