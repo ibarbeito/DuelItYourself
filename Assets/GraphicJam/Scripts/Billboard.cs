59c99ba94cf843d6e37d1f32b3a7da522fa0bea4
JetroD <jetrodopazo48@gmail.com>
2017-09-29 23:10:02 +0200

    Billboard de nombre de jugador

---
+++ Assets/GraphicJam/Scripts/Billboard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour {
+    float initRotation;
+
+	// Use this for initialization
+	void Start () {
+        initRotation = transform.rotation.y;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 newRotation = transform.rotation.eulerAngles;
+        newRotation.y = initRotation;
+
+        transform.rotation = Quaternion.Euler(newRotation);
+
+    }
+}
