e95c0df27359b0e54192918927cbd363bbdcee62
FiloGCS <guillermo.couceiro@gmail.com>
2017-09-30 00:08:54 +0200

    Movidas

---
+++ Assets/GraphicJam/Materials/Foam/splash.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class splash : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ParticleSystem.Particle[] particles = gameObject.GetComponent<ParticleSystem>().GetParticles();
+        foreach (ParticleSystem.Particle particle in collection)
+        {
+
+        }
+		gameObject.GetComponent<ParticleSystem>().GetParticles()
+	}
+}
