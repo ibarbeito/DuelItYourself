a8a5db3f7f39970e1edb96fb52381a17dca3c54f
ibarbeito <ismael.barbeito.vazquez@gmail.com>
2017-09-29 23:09:09 +0200

    Más montaje de selector de personajes. Gogogogogogo

---
+++ Assets/Expositor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Expositor : MonoBehaviour {
+
+	public float SpeedRotation = 1;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.eulerAngles+=new Vector3(0,SpeedRotation,0);
+	}
+}
