7939cc44d1dec7f61c32fb21599eb2454b9582b6
ibarbeito <ismael.barbeito.vazquez@gmail.com>
2017-09-30 01:25:07 +0200

    Últimos retoques del selector

---
+++ Assets/PlayerInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInfo : MonoBehaviour {
+
+	public string Player1Name;
+	public string Player2Name;
+	public Color Player1Color;
+	public Color Player2Color;
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad(this);
+	}
+}
--- Assets/SelectPlayerManager.cs
+++ Assets/SelectPlayerManager.cs
@@ -7,10 +7,16 @@ public class SelectPlayerManager : MonoBehaviour {
 
 	public SelectPlayerController Player1;		
 	public SelectPlayerController Player2;
+	public PlayerInfo PlayerInfo;
+	Color[] Colors = {Color.red, Color.blue, Color.green, Color.yellow};
 	
 	// Update is called once per frame
 	void Update () {
 		if (Player1.Confirmed && Player2.Confirmed) {
+			PlayerInfo.Player1Name = Player1.FinalName;
+			PlayerInfo.Player2Name = Player2.FinalName;
+			PlayerInfo.Player1Color = Colors[Player1.CurrentColor];
+			PlayerInfo.Player2Color = Colors[Player2.CurrentColor];
 			SceneManager.LoadScene("GJ");
 		}
 	}
